8c9cee74986eb6244043920deffd60c069563391
iZeevens <izeevens@gmail.com>
2024-11-27 21:30:03 +0500

    chore: install vue 2, Typescript, eslint, prettier

---
+++ Banki.shop/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <h1>Hello</h1>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+
+export default Vue.extend({
+  name: 'App',
+});
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
